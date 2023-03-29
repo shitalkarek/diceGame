478261e3a2cf7552e2f48cf44e748c3ac1663bd1
Shital karek <118963232+shitalkarek@users.noreply.github.com>
2023-03-30 02:08:12 +0530

    Add files via upload
    dice game by shital karek 
    #how to play 
    
    just refresh the page !

---
+++ index.js
@@ -0,0 +1,39 @@
+//1st !
+
+document.querySelector("h1").innerHTML="refersh me"
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1 ;  //1-6  
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png  -  dice6.png  
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+
+//2nd !
+
+var randomNumber2 = Math.floor(Math.random() *6) + 1 ;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+
+//winning ! 
+
+
+if( randomNumber1  > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!"
+}
+
+else if ( randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "🚩Player 2 Wins!"
+}
+
+else {
+    document.querySelector("h1").innerHTML= "👏Draw!"
+}
